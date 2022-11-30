45d92a77f346d21667c79718984dd43f94674416
Andrey <shuzlik@mail.ru>
2022-11-30 17:54:07 +0300

    second homework

---
+++ task_2.4.py
@@ -0,0 +1,22 @@
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях.
+# Позиции хранятся в файле file.txt в одной строке одно число.
+
+n = int(input('Введите N: '))
+mass = []
+pos = []
+result = 1
+
+for i in range(-n, n + 1):
+    mass.append(i)
+print(mass)
+
+position = open('file.txt', 'r')
+for line in position:
+    pos.append(int(line))
+position.close()
+
+for i in range(len(pos)):
+    if pos[i] <= (n * 2):
+        result *= mass[pos[i]]
+
+print(result)
---
+++ task_2.5.py
@@ -0,0 +1,8 @@
+# Реализуйте алгоритм перемешивания списка.
+import random
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(a)
+random.shuffle(a)
+print(a)
+

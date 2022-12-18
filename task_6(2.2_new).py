7c1dc2a64ed3b5a2ef0215480e31af0987b7c24f
Andrey <shuzlik@mail.ru>
2022-12-18 16:52:48 +0300

    sixth homework

--- task_6(2.2_new).py
+++ task_6(2.2_new).py
@@ -2,6 +2,8 @@
 # Пример:
 # - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
+# С использованием enumerate, map, lambda
+
 number = int(input('Введите число: '))
 
 result = list(enumerate(map(lambda x: 3*x +1, range(1,number + 1))))
